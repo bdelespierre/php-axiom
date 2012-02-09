f862a57f8ca6f6924da5447e0a317b8387d70b1e
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2012-02-09 16:22:15 +0100

    Adding library and database methods to Axiom

--- libraries/axiom/Axiom.class.php
+++ libraries/axiom/Axiom.class.php
@@ -1,8 +1,12 @@
 <?php
 
-class Axiom {
+final class Axiom {
 	
-	protected static $_config;
+	private static $_config;
+	
+	private static $_library;
+	
+	private static $_database;
 	
 	public static function configuration () {
 		if (isset(self::$_config))
@@ -19,4 +23,31 @@ class Axiom {
 		
 		return self::$_config = new $class($file,$section);
 	}
+	
+	public static function library () {
+		if (isset(self::$_library))
+			return self::$_library;
+			
+		list($cache_dir) = func_get_args() + array(AXIOM_APP_PATH . '/ressources/cache');
+		return self::$_library = new axLibrary($cache_dir);
+	}
+	
+	public function database () {
+		if (isset(self::$_database))
+			return self::$_database;
+			
+		$conf = self::configuration();
+		if (!$conf->database)
+			return false;
+			
+		$dsn = "{$conf->database->type}:dbname={$conf->database->database};host={$conf->database->host}";
+		$user = (string)$conf->database->user;
+		$pass = (string)$conf->database->pass;
+		
+		list($driver_options) = func_get_args() + array(array());
+		
+		self::$_database = new PDO($dsn, $user, $pass, $driver_options);
+	}
+	
+	
 }
